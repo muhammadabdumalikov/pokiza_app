7d7ebd57d74b83ab6924836836a3fb0ba2df913d
muhammadabdumalikov <burakali424@gmail.com>
2021-11-11 14:17:25 +0500

    add data from server

--- src/screens/ClientScreens/OrdersScreen/AddOrderScreen/index.jsx
+++ src/screens/ClientScreens/OrdersScreen/AddOrderScreen/index.jsx
@@ -11,6 +11,38 @@ import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
 
 const AddOrderScreen = ({ navigation }) => {
+    const modalState = ({ item }) => {
+        return (
+            <TouchableOpacity
+                style={{ width: "80%", paddingVertical: 15 }}
+                onPress={() => {
+                    setSelectedState(item);
+                    setStateModalVisible(!stateModalVisible);
+                }}
+            >
+                <Text style={{ flex: 1, fontSize: 15, color: "#2196F3" }}>
+                    {item.stateName}
+                </Text>
+            </TouchableOpacity>
+        );
+    };
+
+    const modalRegion = ({ item }) => {
+        return (
+            <TouchableOpacity
+                style={{ width: "80%", paddingVertical: 15 }}
+                onPress={() => {
+                    setSelectedRegion(item);
+                    setRegionModalVisible(!regionModalVisible);
+                }}
+            >
+                <Text style={{ flex: 1, fontSize: 15, color: "#2196F3" }}>
+                    {item.regionName}
+                </Text>
+            </TouchableOpacity>
+        );
+    };
+
     return (
         <View style={styles.containerWrapper}>
             <ScrollView
@@ -22,28 +54,168 @@ const AddOrderScreen = ({ navigation }) => {
                     style={{
                         flex: 6,
                         borderBottomColor: "gray",
-                        borderBottomWidth: .9,
-                        paddingBottom: 20
+                        borderBottomWidth: 0.9,
+                        paddingBottom: 20,
                     }}
                 >
                     <View style={styles.inputContainer}>
-                        <Text>1</Text>
-                        <TextInput />
+                        <Text>Ism:</Text>
+                        <TextInput placeholder="Abdujalilov Abdulaziz" />
                     </View>
                     <View style={styles.inputContainer}>
-                        <Text>1</Text>
-                        <TextInput />
+                        <Text>Telefon:</Text>
+                        <TextInput placeholder="+99896084443" />
                     </View>
                     <View style={styles.inputContainer}>
-                        <Text>1</Text>
-                        <TextInput />
+                        <View style={styles.preTextWrapperStyle}>
+                            <Text style={styles.preText}>State</Text>
+                        </View>
+                        <Modal
+                            animationType="slide"
+                            transparent={true}
+                            visible={stateModalVisible}
+                            onRequestClose={() => {
+                                setStateModalVisible(!stateModalVisible);
+                            }}
+                        >
+                            <View style={styles.centeredView}>
+                                <View style={styles.modalWrapper}>
+                                    <FlatList
+                                        data={states.states}
+                                        renderItem={modalState}
+                                        keyExtractor={(item) => item.stateId}
+                                        contentContainerStyle={styles.modalView}
+                                        style={styles.contenModalView}
+                                        showsVerticalScrollIndicator={false}
+                                    />
+                                </View>
+                                <Pressable
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={() =>
+                                        setStateModalVisible(!stateModalVisible)
+                                    }
+                                >
+                                    <Text style={styles.hideModalButton}>
+                                        Hide Modal
+                                    </Text>
+                                </Pressable>
+                            </View>
+                        </Modal>
+                        <Pressable
+                            style={styles.buttonOpen}
+                            onPress={() => setStateModalVisible(true)}
+                        >
+                            <Text style={styles.textStyle}>
+                                {selectedState != undefined
+                                    ? selectedState.stateName
+                                    : "Add State"}
+                            </Text>
+                        </Pressable>
                     </View>
                     <View style={styles.inputContainer}>
-                        <Text>1</Text>
-                        <TextInput />
+                        <View style={styles.preTextWrapperStyle}>
+                            <Text style={styles.preText}>Address</Text>
+                        </View>
+                        <Modal
+                            animationType="slide"
+                            transparent={true}
+                            visible={regionModalVisible}
+                            onRequestClose={() => {
+                                setRegionModalVisible(!regionModalVisible);
+                            }}
+                        >
+                            <View style={styles.centeredView}>
+                                <View style={styles.modalWrapper}>
+                                    <FlatList
+                                        data={
+                                            regions != undefined
+                                                ? regions.regions
+                                                : []
+                                        }
+                                        renderItem={modalRegion}
+                                        keyExtractor={(item) => item.regionId}
+                                        contentContainerStyle={styles.modalView}
+                                        style={styles.contenModalView}
+                                        showsVerticalScrollIndicator={false}
+                                    />
+                                </View>
+                                <Pressable
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={() =>
+                                        setRegionModalVisible(
+                                            !regionModalVisible
+                                        )
+                                    }
+                                >
+                                    <Text style={styles.hideModalButton}>
+                                        Hide Modal
+                                    </Text>
+                                </Pressable>
+                            </View>
+                        </Modal>
+                        <Pressable
+                            style={styles.buttonOpen}
+                            onPress={() => setRegionModalVisible(true)}
+                        >
+                            <Text style={styles.textStyle}>
+                                {selectedRegion != undefined
+                                    ? selectedRegion.regionName
+                                    : "Add Region"}
+                            </Text>
+                        </Pressable>
                     </View>
                     <View style={styles.inputContainer}>
-                        <Text>1</Text>
+                        <View style={styles.preTextWrapperStyle}>
+                            <Text style={styles.preText}>Address</Text>
+                        </View>
+                        <Modal
+                            animationType="slide"
+                            transparent={true}
+                            visible={regionModalVisible}
+                            onRequestClose={() => {
+                                setRegionModalVisible(!regionModalVisible);
+                            }}
+                        >
+                            <View style={styles.centeredView}>
+                                <View style={styles.modalWrapper}>
+                                    <FlatList
+                                        data={
+                                            regions != undefined
+                                                ? regions.regions
+                                                : []
+                                        }
+                                        renderItem={modalRegion}
+                                        keyExtractor={(item) => item.regionId}
+                                        contentContainerStyle={styles.modalView}
+                                        style={styles.contenModalView}
+                                        showsVerticalScrollIndicator={false}
+                                    />
+                                </View>
+                                <Pressable
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={() =>
+                                        setRegionModalVisible(
+                                            !regionModalVisible
+                                        )
+                                    }
+                                >
+                                    <Text style={styles.hideModalButton}>
+                                        Hide Modal
+                                    </Text>
+                                </Pressable>
+                            </View>
+                        </Modal>
+                        <Pressable
+                            style={styles.buttonOpen}
+                            onPress={() => setRegionModalVisible(true)}
+                        >
+                            <Text style={styles.textStyle}>
+                                {selectedRegion != undefined
+                                    ? selectedRegion.regionName
+                                    : "Add Region"}
+                            </Text>
+                        </Pressable>
+                        <Text>Hudud</Text>
                         <TextInput />
                     </View>
                 </View>
--- src/screens/ClientScreens/OrdersScreen/AddOrderScreen/styles.js
+++ src/screens/ClientScreens/OrdersScreen/AddOrderScreen/styles.js
@@ -21,13 +21,12 @@ const styles = StyleSheet.create({
     infoBox: {
         flex: 2,
         backgroundColor: "red",
-       
     },
     inputContainer: {
         flex: 1,
         width: "100%",
         flexDirection: "row",
-        justifyContent: "space-around",
+        justifyContent: "space-between",
         alignItems: "center",
         borderBottomWidth: 0.5,
         borderBottomColor: "gray",
@@ -56,6 +55,62 @@ const styles = StyleSheet.create({
     sendCodeText: {
         color: "#fff",
         fontSize: 17,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
+    modalWrapper: {
+        width: "70%",
+        height: "30%",
+        shadowColor: "#000",
+        borderRadius: 20,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        backgroundColor: "white",
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        overflow: "hidden",
+        marginBottom: 10 
+    },
+    genderModalWrapper:{
+        height: "15%"
+    },
+    contenModalView: {
+        flex: 1,
+        height: "100%",
+    },
+    modalView: {
+        padding: 16,
+        alignItems: "center", 
+    },
+    buttonOpen: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+        height: 30,
+        width: "33%",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 15,
+        elevation: 2,
+    },
+    textStyle: {
+        color: 'gray',
+        textAlign: "center",
+    },
+    modalText: {
+        textAlign: "center",
+    },
+    hideModalButton: {
+        color: "white"
     },
     fab: {
         width: 64,
