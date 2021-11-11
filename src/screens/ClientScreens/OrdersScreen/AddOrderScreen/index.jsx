f2671baca4d088e71c9d21249883409a13cc1d0f
muhammadabdumalikov <burakali424@gmail.com>
2021-11-11 10:27:36 +0500

    add main common structure

--- src/screens/ClientScreens/OrdersScreen/AddOrderScreen/index.jsx
+++ src/screens/ClientScreens/OrdersScreen/AddOrderScreen/index.jsx
@@ -1,30 +1,48 @@
 import React from "react";
-import { View, Text, ScrollView, TextInput } from "react-native";
+import {
+    View,
+    Text,
+    ScrollView,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+
 import styles from "./styles";
-const AddOrderScreen = () => {
+
+const AddOrderScreen = ({navigation}) => {
     return (
-        <ScrollView>
+        <ScrollView
+            style={styles.container}
+            contentContainerStyle={styles.contentStyle}
+            showsVerticalScrollIndicator={false}
+        >
             <View style={styles.inputContainer}>
                 <Text>1</Text>
-                <TextInput/>
+                <TextInput />
             </View>
-            <View>
+            <View style={styles.inputContainer}>
                 <Text>1</Text>
-                <TextInput/>
+                <TextInput />
             </View>
-            <View>
+            <View style={styles.inputContainer}>
                 <Text>1</Text>
-                <TextInput/>
+                <TextInput />
             </View>
-            <View>
+            <View style={styles.inputContainer}>
                 <Text>1</Text>
-                <TextInput/>
+                <TextInput />
             </View>
-            <View>
+            <View style={styles.inputContainer}>
                 <Text>1</Text>
-                <TextInput/>
+                <TextInput />
             </View>
-                   
+            <TouchableOpacity
+                style={styles.fab}
+                onPress={() => navigation.goBack()}
+            >
+                <Ionicons name="ios-arrow-back" size={28} color="white" />
+            </TouchableOpacity>
         </ScrollView>
     );
 };
--- src/screens/ClientScreens/OrdersScreen/AddOrderScreen/styles.js
+++ src/screens/ClientScreens/OrdersScreen/AddOrderScreen/styles.js
@@ -6,16 +6,56 @@ const height = Dimensions.get("window").height;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: height,
-        backgroundColor: "red"
-
+        width: width,
+        height: "100%",
+        backgroundColor: "#fff",
     },
-    contentContainer: {
-
+    contentStyle: {
+        alignItems: "center",
+        padding: 16,
+        height: "100%",
     },
     inputContainer: {
-        flex: 1
-    }
-})
+        flex: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        borderBottomWidth: .9,
+        borderBottomColor: "gray"
+    },
+    orderBox: {
+        height: height / 6.94,
+        width: width / 1.09,
+        paddingHorizontal: 24,
+        paddingVertical: 14,
+        borderRadius: 10,
+        justifyContent: "space-between",
+        marginBottom: 16,
+        backgroundColor: "#F4F4F5",
+        overflow: "hidden",
+    },
+    orderNumber: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    orderNumberStyle: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    orderNumberAbout: {
+        fontSize: 12,
+    },
+    fab: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        bottom: 28,
+        left: 24,
+        backgroundColor: "#2196F3",
+    },
+});
 
-export default styles
+export default styles;
