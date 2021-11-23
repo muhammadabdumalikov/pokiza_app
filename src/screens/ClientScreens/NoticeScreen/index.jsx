1ae45f10cc9fc2eea31babc47052fdffadb9997f
muhammadabdumalikov <burakali424@gmail.com>
2021-11-23 20:14:43 +0500

    show more

--- src/screens/ClientScreens/NoticeScreen/index.jsx
+++ src/screens/ClientScreens/NoticeScreen/index.jsx
@@ -1,17 +1,55 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native"
+import { View, Text, ScrollView } from "react-native";
+import ShowMoreText from "react-show-more-text";
 
-import {styles} from "./styles"
+import { styles } from "./styles";
 
 const NoticesScreen = () => {
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.noticeBox}>
-                <Text style={styles.statusText}>{"#342484"} carpet is <Text style={styles.statusTextColor}>{"washing"}</Text></Text>
-                <Text style={styles.timeText}>{"1"} hours ago</Text>
-            </View>
+            <ShowMoreText
+                /* Default options */
+                lines={3}
+                more="Show more"
+                less="Show less"
+                className="content-css"
+                anchorClass="my-anchor-css-class"
+                onClick={this.executeOnClick}
+                expanded={false}
+                width={280}
+                truncatedEndingComponent={"... "}
+            >
+                Lorem ipsum dolor sit amet, consectetur{" "}
+                <a
+                    href="https://www.yahoo.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    yahoo.com
+                </a>{" "}
+                adipiscing elit, sed do eiusmod tempor incididunt
+                <a
+                    href="https://www.google.bg/"
+                    title="Google"
+                    rel="nofollow"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    www.google.bg
+                </a>{" "}
+                ut labore et dolore magna amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation
+            </ShowMoreText>
         </ScrollView>
-    )
-}
+    );
+};
 
 export default NoticesScreen;
